a1ddf23bc8db4d078d18b6055e69aabae5b184de
kzhr.sasaki <kzhr.sasaki@gmail.com>
2017-07-14 17:26:46 +0900

    課題再提出
    実行してスコアが表示できているようには見えます。

--- Assets/ScoreController.cs
+++ Assets/ScoreController.cs
@@ -11,42 +11,39 @@ public class ScoreController : MonoBehaviour {
 	//スコア計算用変数
 	private int score = 0;
 
+	//衝突時に呼ばれる関数
+	void OnCollisionEnter(Collision col) {		
+		//変数colTagを宣言
+		string colTag = col.gameObject.tag;
+		if (colTag == "SmallStarTag") {
+			score += 5;
+		} else if (colTag == "LargeStarTag") {
+			score += 20;
+		} else if (colTag == "SmallCloudTag") {
+			score += 25;
+		} else if (colTag == "LargeCloudtag") {
+			score += 50;
+		}
+		//ログ出力
+		Debug.Log("現在のスコア:" + score);
+
+			//ScoreTextにスコアを表示
+		    this.scoreText.GetComponent<Text> ().text = "Score:" + score.ToString();
+	}
+		
 	// Use this for initialization
 	void Start () {
 
 		//初期スコアを代入
 		score = 0;
-		
+
+		//シーン中のScoreTextオブジェクトを取得
+		scoreText = GameObject.Find("ScoreText");
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
-		//OnCollisionEnter関数を呼び出す
-		OnCollisionEnter();
-
-		//ScoreTextにスコアを表示
-		this.scoreText.GetComponent<Text> ().text = "Score:" + score;
-		
-	}
-		
-	//衝突時に呼ばれる関数
-	void OnCollisionEnter(Collision col) {
-
-		//変数colTagを宣言
-		string colTag = col.gameObject.tag;
-
-		if (colTag == "SmallStarTag") {
-			score += 5;
-
-		} else if (colTag == "LargeStarTag") {
-			score += 20;
-
-		} else if (colTag == "SmallCloudTag") {
-			score += 25;
-
-		} else if (colTag == "LargeCloudtag") {
-			score += 50;
-		}
+	void Update () {		
+		//BallにRigidBodyをアタッチしているので、OnCollisionEnter関数を呼び出す処理は不要
+						
 	}		
 }
